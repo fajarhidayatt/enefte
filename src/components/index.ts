425390a95f6936cf431356def7f11bd5e7bc02a2
Fajarhidayatt <fajarhidayat0021@gmail.com>
2023-10-22 20:58:21 +0700

    style(comp): update comps import

---
+++ src/components/index.ts
@@ -0,0 +1,39 @@
+import Button from './Button/Button';
+import ButtonIcon from './Button/ButtonIcon';
+import WalletOption from './Option/WalletOption';
+import Option from './Option/Option';
+import BottomSheet from './Modal/BottomSheet';
+import ModalImage from './Modal/ModalImage';
+import InputText from './Input/InputText';
+import SearhBar from './Input/SearhBar';
+import CollectionCard from './Card/CollectionCard';
+import CategoryCard from './Card/CategoryCard';
+import NftCard from './Card/NftCard';
+import CollectionItem from './Item/CollectionItem';
+import StatsItem from './Item/StatsItem';
+import BoardLayer from './Board/BoardLayer';
+import Header from './Header/Header';
+import BottomTab from './BottomTab/BottomTab';
+import Spacer from './Spacer/Spacer';
+import Profile from './Profile/Profile';
+
+export {
+  BoardLayer,
+  Button,
+  Header,
+  WalletOption,
+  BottomSheet,
+  InputText,
+  NftCard,
+  CollectionItem,
+  BottomTab,
+  SearhBar,
+  CategoryCard,
+  CollectionCard,
+  StatsItem,
+  Option,
+  ButtonIcon,
+  Spacer,
+  Profile,
+  ModalImage,
+};
